191e8029b6c63a0c7e820dd6e8ad47f194d4991d
Pete Gonzalez <4673363+octogonz@users.noreply.github.com>
2022-09-15 17:42:39 -0700

    Scroll to selected item

--- LockfileVisualizer/MainWindow.xaml.cs
+++ LockfileVisualizer/MainWindow.xaml.cs
@@ -1,7 +1,9 @@
 ﻿using ICSharpCode.AvalonEdit.Highlighting;
+using System;
 using System.IO;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Threading;
 using Path = System.IO.Path;
 
 namespace LockfileVisualizer
@@ -12,6 +14,7 @@ namespace LockfileVisualizer
     public partial class MainWindow : Window
     {
         private Lockfile? _Lockfile = null;
+        private readonly DispatcherTimer _updateTimer = new DispatcherTimer();
 
         public MainWindow()
         {
@@ -25,6 +28,13 @@ namespace LockfileVisualizer
             ctlYamlEditor.Options.HighlightCurrentLine = true;
 
             btnReload_Click(this, null);
+
+            txtImportersSearch.Text = "";
+            txtPackagesSearch.Text = "";
+
+            this._updateTimer.Interval = TimeSpan.FromMilliseconds(500);
+            this._updateTimer.Tick += _updateTimer_Tick;
+            this._updateTimer.Start();
         }
 
         private void btnReload_Click(object sender, RoutedEventArgs? e)
@@ -36,10 +46,10 @@ namespace LockfileVisualizer
 
             ctlYamlEditor.Text = this._Lockfile.YamlContent;
 
-            this.RefreshUI();
+            this._refreshFile();
         }
 
-        private void RefreshUI()
+        private void _refreshFile()
         {
             if (this._Lockfile == null)
             {
@@ -62,6 +72,116 @@ namespace LockfileVisualizer
                 item.Tag = x;
                 item.Content = x.WorkspacePath;
                 ctlImportersListView.Items.Add(item);
+            }
+        }
+
+        private void _requestUpdateUI()
+        {
+            this._updateTimer.Start();
+        }
+
+        private void _updateTimer_Tick(object sender, EventArgs e)
+        {
+            this._updateTimer.Stop();
+            this._updateUI();
+        }
+
+        private void _updateUI()
+        {
+            ctlImportersListView.Items.Filter = null;
+            ctlPackagesListView.Items.Filter = null;
+
+            ctlImportersListView.Items.Filter = ctlImportersListView_ItemsFilter;
+            ctlPackagesListView.Items.Filter = ctlPackagesListView_ItemsFilter;
+        }
+
+        private bool ctlImportersListView_ItemsFilter(object itemObject)
+        {
+            var item = itemObject as ListViewItem;
+            if (item != null)
+            {
+                string text = txtImportersSearch.Text.Trim();
+                if (!string.IsNullOrEmpty(text))
+                {
+                    LockfileImporter? importer = item.Tag as LockfileImporter;
+                    if (importer != null)
+                    {
+                        if (importer.WorkspacePath.IndexOf(text, System.StringComparison.InvariantCultureIgnoreCase) >= 0)
+                        {
+                            return true;
+                        }
+                        else
+                        {
+                            return false;
+                        }
+                    }
+                }
+            }
+            return true;
+        }
+
+        private bool ctlPackagesListView_ItemsFilter(object itemObject)
+        {
+            var item = itemObject as ListViewItem;
+            if (item != null)
+            {
+                string text = txtPackagesSearch.Text.Trim();
+                if (!string.IsNullOrEmpty(text))
+                {
+                    LockfilePackage? package = item.Tag as LockfilePackage;
+                    if (package != null)
+                    {
+                        if (package.PackagePath.IndexOf(text, System.StringComparison.InvariantCultureIgnoreCase) >= 0)
+                        {
+                            return true;
+                        }
+                        else
+                        {
+                            return false;
+                        }
+                    }
+                }
+            }
+            return true;
+        }
+
+        private void txtPackagesSearch_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            this._requestUpdateUI();
+        }
+
+        private void txtImportersSearch_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            this._requestUpdateUI();
+        }
+
+        private void ctlPackagesListView_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            var item = ctlPackagesListView.SelectedItem as ListViewItem;
+            if (item != null)
+            {
+                LockfilePackage? package = item.Tag as LockfilePackage;
+                if (package != null)
+                {
+                    ctlYamlEditor.ScrollToLine(package.LineNumber - 1);
+                    ctlYamlEditor.TextArea.Caret.Line = package.LineNumber - 1;
+                    ctlYamlEditor.TextArea.Caret.BringCaretToView();
+                }
+            }
+        }
+
+        private void ctlImportersListView_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            var item = ctlImportersListView.SelectedItem as ListViewItem;
+            if (item != null)
+            {
+                LockfileImporter? importer = item.Tag as LockfileImporter;
+                if (importer != null)
+                {
+                    ctlYamlEditor.ScrollToLine(importer.LineNumber - 1);
+                    ctlYamlEditor.TextArea.Caret.Line = importer.LineNumber - 1;
+                    ctlYamlEditor.TextArea.Caret.BringCaretToView();
+                }
             }
         }
     }
